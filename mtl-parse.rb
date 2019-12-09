50cb516721eb22b76f7d714288ddb3ee68d12764
Piotr Marszalik <addenial@users.noreply.github.com>
2019-12-09 17:51:37 -0500

    Update mtl-parse.rb

--- mtl-parse.rb
+++ mtl-parse.rb
@@ -1,5 +1,11 @@
 #!/usr/bin/ruby
 
+
+`ruby nmap_parse.rb -i results_TCP_Top200Ports_sS_sV.xml > mtl.csv`
+`ruby nmap_parse.rb -i results_UDP_Top1024Ports_sU_sV.xml >> mtl.csv`
+`ruby nmap_parse.rb -i results_TCP_full.xml >> mtl.csv`
+
+
 #unique open ports across all, pulled from mtl
 `cat mtl.csv | grep -vF tcpwrapped | cut -d, -f2 | sort | uniq > ports.txt`
 
@@ -10,4 +16,3 @@ File.readlines(ports).each do |line|
   puts line.chomp
   `awk '/#{line.chomp}\\/open/ {print $2}' *.gnmap | sort | uniq > #{line.chomp}.open`
 end
-
